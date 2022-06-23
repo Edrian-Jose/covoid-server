1704c7b315fddffa2fa6d1605b76d3420d40ce55
Edrian-Jose <edrianjoseferrer@gmail.com>
2022-06-23 15:00:05 +0800

    added module configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// NestJS Modules
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BullModule } from '@nestjs/bull';
+import { JwtModule } from '@nestjs/jwt';
+
+// Covoid Modules
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { StorageModule } from './storage/storage.module';
@@ -8,7 +16,40 @@ import { DetectorModule } from './detector/detector.module';
 import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, StorageModule, DetectorModule, DataModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        redis: {
+          host: config.get<string>('REDIS_HOST'),
+          port: config.get<number>('REDIS_PORT'),
+          password: config.get<string>('REDIS_PASS'),
+        },
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+        dbName: 'HPM',
+      }),
+    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60s' },
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+    StorageModule,
+    DetectorModule,
+    DataModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
